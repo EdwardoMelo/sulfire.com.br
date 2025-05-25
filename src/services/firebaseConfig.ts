0fa76b5d73a1342a64b66f94ab18f645299412b6
ti-dolphin <ti.dse01@gmail.com>
2025-05-25 16:28:00 -0300

    sem .env

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,19 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDa76oK-yiFWbHvieqkX3P_0gLNg4vCS28",
-  authDomain: "marine-actor-459701-f3.firebaseapp.com",
-  projectId: "marine-actor-459701-f3",
-  storageBucket: "marine-actor-459701-f3.firebasestorage.app",
-  messagingSenderId: "128835999273",
-  appId: "1:128835999273:web:3dd47ffd36cba07aa02e38",
-  measurementId: "G-DR4YR1GGDM",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:  import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:  import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:  import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId:  import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
